957a6023e656aa345719d4273e9eb6dad5f7c2c6
Kyle Graehl <kgraehl@gmail.com>
2014-02-05 17:38:27 -0800

    fix

--- js/index.js
+++ js/index.js
@@ -8,17 +8,19 @@ function ondownloadclick() {
 function onload() {
 
     var elts = document.querySelectorAll('.js-download')
-    for (var i=0; i<elts.length; i++) {
-        elts.addEventListener('click', function(evt) {
-            var _this = this
-            ga('send','event','Clicks','Download', {
-                'hitCallback': function() {
-                    console.log('sent clicks download event',this)
-                    debugger
-                }
+    if (elts) {
+        for (var i=0; i<elts.length; i++) {
+            elts[i].addEventListener('click', function(evt) {
+                var _this = this
+                ga('send','event','Clicks','Download', {
+                    'hitCallback': function() {
+                        console.log('sent clicks download event',this)
+                        debugger
+                    }
+                });
+                evt.preventDefault()
+                return false
             });
-            evt.preventDefault()
-            return false
-        });
+        }
     }
 }
