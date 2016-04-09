341f175b9556198fbde4c64bd8fb54337af65b5f
Kyle Graehl <kgraehl@gmail.com>
2016-04-08 18:35:46 -0700

    better and simpler magnet setup page

---
+++ magnet/index.js
@@ -0,0 +1,23 @@
+function getel(id) {return document.getElementById(id)}
+function ondom() {
+    console.log('dom loaded')
+
+}
+    registerHandler()
+
+function registerHandler() {
+    var handlerPath = "/share/"
+    var handlerArgument = "#magnet_uri="
+    var handler
+    var handlerFullUrl = window.location.origin + handlerPath + handlerArgument
+    var Protocol = "magnet"
+    var ARGS = [Protocol,
+                handlerFullUrl + "%s",
+                "JSTorrent"]
+    console.log('request register handler',ARGS)
+    navigator.registerProtocolHandler(ARGS[0],ARGS[1],ARGS[2]);
+}
+
+
+
+document.addEventListener("DOMContentLoaded",ondom)
